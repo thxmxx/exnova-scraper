1fdf0ea094aa10e6ee869d9c86cd4bdb14f2ad72
Lucas Thom <lucas@motora.ai>
2024-05-10 01:18:37 -0300

    updates

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,106 +1,199 @@
 import {Page, Browser, launch} from 'puppeteer';
 import {Config} from '../config/config';
+import {Subject} from 'rxjs';
+
+export type Candle = {
+  active_id: number;
+  size: number;
+  at: number;
+  from: number;
+  to: number;
+  id: number;
+  open: number;
+  close: number;
+  min: number;
+  max: number;
+  ask: number;
+  bid: number;
+  volume: number;
+  phase: string;
+};
+
+enum SuppportedMessages {
+  CANDLE_GENERATED = 'candle-generated',
+  INITIALIZATION_DATA = 'initialization-data',
+  FIRST_CANDLES = 'first-candles',
+  GET_CANDLES = 'get-candles',
+  CANDLES = 'candles',
+}
 
 export class Client {
   private browser: Browser | null = null;
   private page: Page | null = null;
+  private actives = new Map<number, {name: string; requestId: string}>();
+  public isRunning = false;
+  public candles = new Subject<{
+    topic: string;
+    active: string;
+    candles: Candle[];
+  }>();
+  public updates = new Subject<{
+    topic: string;
+    active: string;
+    candle: Candle;
+  }>();
 
   constructor() {
     console.log('Client constructor');
   }
 
   async setupWebSocketListening(page: Page) {
+    if (!page) {
+      throw new Error('[setupWebSocketListening] Page is null!');
+    }
     const client = await page.createCDPSession();
     await client.send('Network.enable');
 
-    let messages = 1000;
-    let actives = {};
     const fs = require('fs');
     // Manipular mensagens de WebSocket
     client.on('Network.webSocketFrameReceived', (event: any) => {
       const data = JSON.parse(event.response.payloadData);
-      if (data.name === 'initialization-data') {
-        const actives = data.msg.binary.actives;
-        fs.writeFileSync('actives.json', JSON.stringify(actives, null, 2));
+      const msg = data.msg;
+      const name = data.name;
+      const requestId = data.request_id;
+      // candle-generated
+      switch (name) {
+        case SuppportedMessages.CANDLE_GENERATED:
+          const active = this.actives.get(data.msg.active_id);
+          if (!active) {
+            console.error('Active not found:', data.msg.active_id);
+            return;
+          }
+          const obj = {
+            topic: name,
+            active: active.name || 'UNKNOWN',
+            candle: msg,
+          };
+          this.updates.next(obj);
+          break;
+        case SuppportedMessages.INITIALIZATION_DATA:
+          {
+            const actives = data.msg.binary.actives;
+            Object.keys(actives).forEach(key => {
+              this.actives.set(actives[key].id, {
+                name: actives[key].ticker,
+                requestId: actives[key]?.request_id || 0,
+              });
+            });
+            // console.log('Actives:', this.actives);
+          }
+          break;
+        case SuppportedMessages.CANDLES:
+          {
+            const active = Array.from(this.actives.values()).find(
+              v => v.requestId == requestId
+            );
+            if (!active) {
+              console.error('Active not found:', requestId);
+              return;
+            }
+            this.candles.next({
+              topic: name,
+              active: active?.name || 'UNKNOWN',
+              candles: msg.candles,
+            });
+          }
+          break;
+        default:
+          // console.log('Mensagem recebida:', JSON.stringify(data, null, 2));
+          break;
       }
-      if (messages > 0) {
-        fs.appendFileSync('messages.txt', JSON.stringify(data, null, 2));
-        messages--;
+    });
+
+    // Manipular mensagens de WebSocket enviadas
+    client.on('Network.webSocketFrameSent', (event: any) => {
+      const data = JSON.parse(event.response.payloadData);
+      const msg = data.msg;
+      const name = data.name;
+      const requestId = data.request_id;
+      if (msg.name == SuppportedMessages.GET_CANDLES) {
+        const active_id = msg.body.active_id;
+        const active = this.actives.get(active_id);
+        this.actives.set(active_id, {
+          name: active?.name || '',
+          requestId,
+        });
       }
-      console.log('Mensagem recebida:', JSON.stringify(data, null, 2));
     });
   }
 
+  close() {
+    if (this.browser) {
+      this.browser.close();
+      this.isRunning = false;
+    }
+  }
+
   async start() {
-    this.browser = await launch({headless: false});
-    this.page = await this.browser.newPage();
+    try {
+      this.browser = await launch({
+        headless: false,
+      });
 
-    console.log(`USERNAME: ${Config.userName} PASSWORD: ${Config.password}`);
+      do {
+        await this.tryLogin();
+      } while (!(await this.isLoggedIn()));
 
-    await this.setupWebSocketListening(this.page);
+      if (!this.page) throw new Error('[start] Page is null');
 
-    this.page.goto('https://trade.exnova.com/traderoom');
+      await this.setupWebSocketListening(this.page);
 
-    // the username input field is #root > div > div > div > div > form > div:nth-child(1) > div > div > input
-    const userNameInput = await this.page.waitForSelector(
-      '#root > div > div > div > div > form > div:nth-child(1) > div > div > input'
-    );
-    await userNameInput?.type(Config.getUserName());
+      console.log('Client start');
+      this.isRunning = true;
+    } catch (error) {
+      console.error('Error trying to start client', error);
+      this.isRunning = false;
+    }
+  }
 
-    // the password input field is #root > div > div > div > div > form > div:nth-child(2) > div > div > input
-    const passwordInput = await this.page.waitForSelector(
-      '#root > div > div > div > div > form > div:nth-child(2) > div > div > input'
-    );
-    await passwordInput?.type(Config.getPassword());
+  async tryLogin() {
+    try {
+      if (!this.browser) throw new Error('Browser is null');
 
-    // the login button is #root > div > div > div > div > form > button
-    const loginButton = await this.page.waitForSelector(
-      '#root > div > div > div > div > form > button'
-    );
-    await loginButton?.click();
+      this.page = await this.browser.newPage();
 
-    await this.page.setRequestInterception(true);
+      this.page.goto('https://trade.exnova.com/traderoom');
 
-    this.page.on('request', (interceptedRequest: any) => {
-      if (
-        interceptedRequest.url().includes('websocket') &&
-        interceptedRequest.resourceType() != 'image'
-      ) {
-        console.log(
-          'A request was made:',
-          interceptedRequest.url(),
-          interceptedRequest.resourceType()
-        );
-      }
-      // Filtrar apenas as solicitações de WebSocket
-      if (interceptedRequest.resourceType() === 'websocket') {
-        interceptedRequest.continue();
+      // the username input field is #root > div > div > div > div > form > div:nth-child(1) > div > div > input
+      const userNameInput = await this.page.waitForSelector(
+        '#root > div > div > div > div > form > div:nth-child(1) > div > div > input'
+      );
+      await userNameInput?.type(Config.getUserName());
 
-        // Manipular mensagens WebSocket
-        interceptedRequest.on('websocket', (ws: any) => {
-          console.log('Nova conexão WebSocket:', ws.url());
+      // the password input field is #root > div > div > div > div > form > div:nth-child(2) > div > div > input
+      const passwordInput = await this.page.waitForSelector(
+        '#root > div > div > div > div > form > div:nth-child(2) > div > div > input'
+      );
+      await passwordInput?.type(Config.getPassword());
 
-          // Manipular mensagens recebidas
-          ws.on('message', (message: any) => {
-            console.log('Mensagem recebida:', message);
-            // Aqui você pode processar ou manipular as mensagens conforme necessário
-          });
+      // the login button is #root > div > div > div > div > form > button
+      const loginButton = await this.page.waitForSelector(
+        '#root > div > div > div > div > form > button'
+      );
+      await loginButton?.click({delay: 1000, clickCount: 3});
 
-          ws.on('close', () => {
-            console.log('Conexão WebSocket fechada');
-          });
-        });
-      } else {
-        interceptedRequest.continue();
-      }
-    });
+      // start trading button is #root > div.css-3uqybw.ep6b6450 > header > div > div > div.css-cea5o1.e1t2y47p4 > button > span
+      const startTradingButton = await this.page.waitForSelector(
+        '#root > div.css-3uqybw.ep6b6450 > header > div > div > div.css-cea5o1.e1t2y47p4 > button > span'
+      );
+      await startTradingButton?.click();
+    } catch (error) {
+      console.error('Error trying to login', error);
+    }
+  }
 
-    // start trading button is #root > div.css-3uqybw.ep6b6450 > header > div > div > div.css-cea5o1.e1t2y47p4 > button > span
-    const startTradingButton = await this.page.waitForSelector(
-      '#root > div.css-3uqybw.ep6b6450 > header > div > div > div.css-cea5o1.e1t2y47p4 > button > span'
-    );
-    await startTradingButton?.click();
-
-    console.log('Client start');
+  async isLoggedIn() {
+    const depositButton = await this.page?.waitForSelector('#glcanvas');
+    return depositButton !== null;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,59 @@
-import { Client } from "./client/client";
+import {Subscription} from 'rxjs';
+import {Client} from './client/client';
+import * as WebSocket from 'ws';
+
+const wss = new WebSocket.Server({port: 8080});
 
 const main = async () => {
-    const client = new Client();
-    client.start();
-}
+  const client = new Client();
+
+  let updatesSubscription: Subscription | null = null;
+  let candlesSubscription: Subscription | null = null;
+
+  wss.on('connection', async (ws: WebSocket) => {
+    console.log('Cliente conectado');
+
+    ws.on('message', async (message: Buffer) => {
+      console.log('Mensagem recebida:', message);
+
+      switch (message.toString()) {
+        case 'start':
+          updatesSubscription = client.updates.subscribe(
+            ({topic, active, candle}) => {
+              try {
+                ws.send(JSON.stringify({topic, active, candle}));
+              } catch (e) {
+                console.error('Erro ao enviar mensagem:', e);
+              }
+            }
+          );
+          candlesSubscription = client.candles.subscribe(
+            ({topic, active, candles}) => {
+              try {
+                ws.send(JSON.stringify({topic, active, candles}));
+              } catch (e) {
+                console.error('Erro ao enviar mensagem:', e);
+              }
+            }
+          );
+          await client.start();
+          break;
+        case 'ping':
+          ws.send(JSON.stringify({topic: 'pong', _t: Date.now()}));
+          break;
+        case 'close':
+          client.close();
+          ws.close();
+          break;
+      }
+    });
+
+    ws.on('close', () => {
+      (updatesSubscription as Subscription)?.unsubscribe();
+      (candlesSubscription as Subscription)?.unsubscribe();
+      console.log('[WS] Cliente desconectado');
+    });
+  });
+};
 
 main();
